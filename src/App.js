de33a1d0f341377d5114fbf8f623daea3e57f956
Chris Van Pelt <vanpelt@gmail.com>
2019-04-03 22:26:14 -0400

    Initial commit for emotions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,173 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Webcam from "react-webcam";
+import * as faceapi from "face-api.js";
+import * as tf from "@tensorflow/tfjs";
+import { extractFaceAndCrop } from "./util.js";
+import "./App.css";
 
 class App extends Component {
+  state = { loading: true, avg: [] };
+  upload = React.createRef();
+  webcam = React.createRef();
+  preview = React.createRef();
+  labels = ["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise", "Neutral"];
+
+  /**
+   * Load the face detector and smile models
+   */
+  async componentDidMount() {
+    this.tinyFace = await faceapi.loadTinyFaceDetectorModel(
+      process.env.PUBLIC_URL + "/models"
+    );
+    this.smileDetector = await tf.loadLayersModel(
+      process.env.PUBLIC_URL + "models/model.json"
+    );
+    const tensor = await extractFaceAndCrop(this.getInput());
+    this.smileDetector.predict(tensor);
+    this.setState({ loading: false });
+  }
+
+  /**
+   * Preview the input given to the model
+   */
+  previewInput(input) {
+    const ctx = this.preview.current.getContext("2d");
+    const result = ctx.createImageData(48, 48);
+    for (var i = 0; i < input.length; i += 1) {
+      result.data[i * 4] = input[i] * 255;
+      result.data[i * 4 + 1] = input[i] * 255;
+      result.data[i * 4 + 2] = input[i] * 255;
+      result.data[i * 4 + 3] = 255;
+    }
+    ctx.putImageData(result, 0, 0);
+  }
+
+  /**
+   * Use the loaded model to make a prediction
+   */
+  async predict(input) {
+    const detection = await faceapi.detectSingleFace(
+      input,
+      new faceapi.TinyFaceDetectorOptions({ inputSize: 256 })
+    );
+    if (detection) {
+      const tensor = await extractFaceAndCrop(input, detection);
+      this.previewInput(tensor.dataSync());
+      this.setState({ loading: true });
+      const start = window.performance.now();
+      const results = this.smileDetector.predict(tensor).dataSync();
+      const time = window.performance.now() - start;
+      this.setState({
+        loading: false,
+        inferenceTime: time,
+        avg: this.state.avg.concat(time),
+        results
+      });
+    } else {
+      console.warn("No face found, skipping dection");
+    }
+  }
+
+  /**
+   * Run inference every 200ms
+   */
+  toggleAutoDetect() {
+    if (!this.state.autoDetect) {
+      this.setState({
+        autoDetect: setInterval(() => {
+          this.predict(this.getInput());
+        }, 200)
+      });
+    } else {
+      clearInterval(this.state.autoDetect);
+      this.setState({ autoDetect: false });
+    }
+  }
+
+  async uploadImage() {
+    const imgFile = this.upload.current.files[0];
+    const img = await faceapi.bufferToImage(imgFile);
+  }
+
+  getInput() {
+    let input = this.webcam.current.getCanvas();
+    if (!input) {
+      const canvas = document.createElement("canvas");
+      canvas.width = 100;
+      canvas.height = 100;
+      canvas.getContext("2d").fillRect(0, 0, 100, 100);
+      input = canvas;
+    }
+    //TODO: handle no webcam
+    return input;
+  }
+
   render() {
+    const videoConstraints = {
+      facingMode: "user"
+    };
+    let results = [];
+    if (this.state.inferenceTime) {
+      let best = 0;
+      let bestIdx = 0;
+      this.state.results.forEach((r, i) => {
+        if (r > best) {
+          best = r;
+          bestIdx = i;
+        }
+      });
+      this.state.results.forEach((r, i) =>
+        results.push(
+          <li key={i} className={i == bestIdx ? "best" : ""}>
+            {this.labels[i]}: {Math.round(r * 10000) / 100}%
+          </li>
+        )
+      );
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header
+          className={"App-header" + (this.state.loading ? " App-loading" : "")}
+        >
+          <Webcam videoConstraints={videoConstraints} ref={this.webcam} />
+          <div className="App-controls">
+            <button
+              className="App-detect"
+              onClick={() => this.predict(this.getInput())}
+            >
+              Predict Once
+            </button>
+            <button
+              className="App-detect"
+              onClick={() => this.toggleAutoDetect()}
+            >
+              {this.state.autoDetect ? "Stop " : "Start "}Streaming
+            </button>
+            <div className="App-stats">
+              {this.state.inferenceTime && (
+                <ul>
+                  <li>
+                    <strong>Average time:</strong>{" "}
+                    {Math.round(
+                      (this.state.avg.reduce((r, el) => r + el, 0) /
+                        this.state.avg.length) *
+                        100
+                    ) / 100}
+                    ms
+                  </li>
+                  <li>
+                    <strong>Last inference time:</strong>{" "}
+                    {Math.round(this.state.inferenceTime * 100) / 100}ms
+                  </li>
+                  <li>
+                    <strong>Results:</strong>
+                    <ul>{results}</ul>
+                  </li>
+                </ul>
+              )}
+            </div>
+            <canvas ref={this.preview} width="48" height="48" />
+          </div>
         </header>
       </div>
     );
---
+++ src/util.js
@@ -0,0 +1,40 @@
+import * as faceapi from "face-api.js";
+import * as tf from "@tensorflow/tfjs";
+
+const cropImage = img => {
+  const size = Math.min(img.shape[0], img.shape[1]);
+  const centerHeight = img.shape[0] / 2;
+  const beginHeight = centerHeight - size / 2;
+  const centerWidth = img.shape[1] / 2;
+  const beginWidth = centerWidth - size / 2;
+  return img.slice([beginHeight, beginWidth, 0], [size, size, 3]);
+};
+
+/**
+ * Extract a face from an image given raw input and a
+ * detection.  Resize to a square given widthHeight.
+ * Convert to a tensor and normalize.
+ */
+export const extractFaceAndCrop = async (
+  input,
+  detection,
+  widthHeight = 48
+) => {
+  let face = input;
+  if (detection) {
+    face = (await faceapi.extractFaces(input, [detection]))[0];
+  }
+  const canvas = document.createElement("canvas");
+  canvas.width = widthHeight;
+  canvas.height = widthHeight;
+  canvas.getContext("2d").drawImage(face, 0, 0, widthHeight, widthHeight);
+  const colorTensor = tf.browser.fromPixels(canvas);
+  const grayscaleTensor = colorTensor.mean(2).expandDims(2);
+  //TODO: deal with aspect ratio and cropping
+  //const croppedTensor = cropImage(grayscaleTensor);
+  //TODO: don't normalize and show the class
+  return grayscaleTensor
+    .expandDims(0)
+    .toFloat()
+    .div(tf.scalar(255));
+};
